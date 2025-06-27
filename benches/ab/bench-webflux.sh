8a14bb22775c4c59a9a3acfce01d08b666fd421e
bernardo <tgavent3@gmail.com>
2025-06-27 12:51:00 +0100

    re-add freemarker to webflux bench script

--- benches/ab/bench-webflux.sh
+++ benches/ab/bench-webflux.sh
@@ -50,8 +50,8 @@ ROUTES=(
    presentations/jstachio/virtualSync
 #   presentations/pebble/sync
 #   presentations/pebble/virtualSync
-#   presentations/freemarker/sync
-#   presentations/freemarker/virtualSync
+   presentations/freemarker/sync
+   presentations/freemarker/virtualSync
    presentations/trimou/sync
    presentations/trimou/virtualSync
 #   presentations/velocity/sync
@@ -72,8 +72,8 @@ ROUTES=(
    stocks/jstachio/virtualSync
 #   stocks/pebble/sync
 #   stocks/pebble/virtualSync
-#   stocks/freemarker/sync
-#   stocks/freemarker/virtualSync
+   stocks/freemarker/sync
+   stocks/freemarker/virtualSync
    stocks/trimou/sync
    stocks/trimou/virtualSync
 #   stocks/velocity/sync
